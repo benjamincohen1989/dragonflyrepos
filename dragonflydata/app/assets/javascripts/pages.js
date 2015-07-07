3688acb95a2bea0b19f748b3a96631269cd8224e
benjamincohen1989 & Thomas Goldenberg <pair+benjamincohen1989.tgoldenberg@devbootcamp.com>
2015-07-07 15:55:36 -0400

    add all members of NYC Dragonflies

--- dragonflydata/app/assets/javascripts/pages.js
+++ dragonflydata/app/assets/javascripts/pages.js
@@ -1,6 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function() {
+
+  // navigation to respond to screen resizing
   var menuToggle = $('#js-mobile-menu').unbind();
   $('#js-navigation-menu').removeClass("show");
 
@@ -12,26 +14,28 @@ $(document).ready(function() {
       }
     });
   });
-  var totalRepos = [];
-  var usernames = ["tgoldenberg", "alcsatt", "benjamin.cohen1989", "tekd", "JMC11", "sevennote", "julia-castro", "lowellmower", "mcardacci", "michaelbbozza", "grapefruittricky", "shmartin", "sixthand6th"];
+
+  // AJAX call to get most recent repositories of DBC students
+  var usernames = ["tgoldenberg", "alcsatt", "benjamincohen1989", "tekd", "JMC11", "sevennote", "julia-castro", "lowellmower", "mcardacci", "michaelbbozza", "grapefruitricky", "shmartin", "sixthand6th", "johnlyden"];
   var githubUrl = "https://api.github.com/users/";
   $('.btn-lg').on('click', function(e) {
     e.preventDefault();
     usernames.forEach(function(element, idx) {
       var username = element;
+      var index = idx + 1;
 
       $.ajax({
-        url: githubUrl + element + "/repos?sort=created?access_token=e700c6f603d1ac7881c31d703e315e22bb044ddc",
+        url: githubUrl + username + "/repos?sort=created&access_token=a99ebfa9324ccb18ed08527715e659e3d8e339f8",
         method: "get",
-        dataType: "json",
-        data: {usernames: usernames}
+        dataType: "json"
       }).done(function(data) {
-        var element = '<h4>'+ username + '</h4><hr/>';
+        var element = '<div class="user-profile"><div class="user-avatar"><img src="' + data[0]["owner"]["avatar_url"] + '"/></div><div class="user-info"><hr/><h4>'+ username + '</h4>';
         data.forEach(function(repo, idx) {
           if (idx < 3) {
-            element += '<p><a href="' + repo["html_url"] + '">' + repo["name"] + " || " + repo["created_at"] + "</a></p>";
+            element += '<p><a href="' + repo["html_url"] + '">' + repo["name"] + "</a> || created on: " + new Date(repo["created_at"]).toLocaleDateString() + "</p>";
           }
         });
+        element += "</div></div>";
         $('.display-panel').append(element);
 
       }).fail(function(error) {
